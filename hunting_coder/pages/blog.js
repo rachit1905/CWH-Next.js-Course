0c262ef28c75bcac5717817ac077183c3ee15d0d
rachit1905 <rachitpagarwal@gmail.com>
2022-04-15 18:37:28 +0530

    Add content and style to blog and blogpost page

--- hunting_coder/pages/blog.js
+++ hunting_coder/pages/blog.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div className={styles.grid}>
+          <a href="/blogpost/learn-react" className={styles.card}>
+            <h3 className={styles.blogItemh3}>
+              How to learn Javascript in 2022?
+            </h3>
+            <p>JavaScript is a language used to design logic for the web</p>
+          </a>
 
-export default Blog
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3 className={styles.blogItemh3}>
+              How to learn Javascript in 2022?
+            </h3>
+            <p>JavaScript is a language used to design logic for the web</p>
+          </a>
+
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3 className={styles.blogItemh3}>
+              How to learn Javascript in 2022?
+            </h3>
+            <p>JavaScript is a language used to design logic for the web</p>
+          </a>
+
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3 className={styles.blogItemh3}>
+              How to learn Javascript in 2022?
+            </h3>
+            <p>JavaScript is a language used to design logic for the web</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
--- hunting_coder/pages/blogpost/[slug].js
+++ hunting_coder/pages/blogpost/[slug].js
@@ -1,12 +1,24 @@
-import React from 'react'
-import {useRouter} from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
-    const router = useRouter();
-    const {slug} = router.query;
+  const router = useRouter();
+  const { slug } = router.query;
   return (
-    <div>{slug}</div>
-  )
-}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+          temporibus laborum consequuntur. Deserunt quisquam fugiat quo facere
+          cumque velit blanditiis temporibus, officia qui quasi quod vel sint
+          corrupti aut asperiores!
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default slug
+export default slug;
